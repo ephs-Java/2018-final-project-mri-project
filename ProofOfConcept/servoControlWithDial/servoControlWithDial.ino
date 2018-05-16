2ca25ba7a191620f80851fd5844c22f168a492f4
Isaac Ash-Johnson <isaacashj@gmail.com>
2018-05-16 08:58:31 -0500

    Rudder Proof-of-Concept
    Servo is controlled by rotations of a potentiometer.
    Could be used in applications such as rotating a plastic rudder for steering.

---
+++ ProofOfConcept/servoControlWithDial/servoControlWithDial.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+
+Servo servo;
+int potentiometerPin = 0;
+int val;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(potentiometerPin);
+  val = map(val, 0, 1023, 0, 180);
+  servo.write(val);
+  delay(10);
+}
